653e01b12c7d3a4899ee86c9baeda812867e1145
pcysl5edgo <pcysl5edgo@gmail.com>
2019-09-20 22:59:37 +0900

    [update]

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitHubActionsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static string X()
+            {
+                return "\"Hello World! Dotnet Test!!!\"";
+            }
+            Console.WriteLine(X());
+        }
+    }
+}
